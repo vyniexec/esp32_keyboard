bd957914c7d9692f620f75ff06816facd08f7f20
Vinicius Santos <109113022+vyniexec@users.noreply.github.com>
2023-04-03 20:28:46 -0300

    Projeto em desenvolvimento teclado matricial

--- main/keyboardtest.c
+++ main/keyboardtest.c
@@ -5,12 +5,12 @@
 #include "esp_log.h"
 
 // -- Nomeando as saídas e entradas do ESP32 --;
-#define SH GPIO_NUM_2       // -- Pino do registrador de deslocamento (74HC595) - SERIAL IN --;
-#define CLK GPIO_NUM_4      // -- Pino do registrador de deslocamento (74HC595) - CLOCK --;
-#define WR GPIO_NUM_16      // -- Pino do registrador de deslocamento (74HC595) - LATCH --;
-#define RD GPIO_NUM_15      // -- Pino do registrador de deslocamento (74HC165) - SH/LD --;
-#define column 4            // -- Definindo 4 colunas --;
-#define rows 4              // -- Definindo 4 linhas --;
+#define SH GPIO_NUM_2                       // -- Pino do registrador de deslocamento (74HC595) - SERIAL IN --;
+#define CLK GPIO_NUM_4                      // -- Pino do registrador de deslocamento (74HC595) - CLOCK --;
+#define WR GPIO_NUM_16                      // -- Pino do registrador de deslocamento (74HC595) - LATCH --;
+#define RD GPIO_NUM_15                      // -- Pino do registrador de deslocamento (74HC165) - SH/LD --;
+#define column 4                            // -- Definindo 4 colunas --;
+#define rows 4                              // -- Definindo 4 linhas --;
 
 // -- Defininições de variaveis --;
 static const char *TAG = "@vyniexec";
@@ -25,10 +25,10 @@ const uint8_t teclas[rows][column] = {
 // -- Inicializando e limpando as portas do teclado --;
 void keyboardInit(void)
 {
-    //gpio_reset_pin(SH);                 // -- Resetando todos os pinos a serem usados --;
-    //gpio_reset_pin(CLK);                // -- Resetando todos os pinos a serem usados --;
-    //gpio_reset_pin(WR);                 // -- Resetando todos os pinos a serem usados --;
-    //gpio_reset_pin(RD);                 // -- Resetando todos os pinos a serem usados --;
+    //gpio_reset_pin(SH);                   // -- Resetando todos os pinos a serem usados --;
+    //gpio_reset_pin(CLK);                  // -- Resetando todos os pinos a serem usados --;
+    //gpio_reset_pin(WR);                   // -- Resetando todos os pinos a serem usados --;
+    //gpio_reset_pin(RD);                   // -- Resetando todos os pinos a serem usados --;
 
     // -- Configurandos as portas --;
     esp_rom_gpio_pad_select_gpio(SH);
@@ -40,10 +40,10 @@ void keyboardInit(void)
     esp_rom_gpio_pad_select_gpio(RD);
     gpio_set_direction(RD, GPIO_MODE_INPUT);
 
-    gpio_set_level(SH, 0);              // -- Zerando as portas --;
-    gpio_set_level(CLK, 0);             // -- Zerando as portas --;
-    gpio_set_level(WR, 0);              // -- Zerando as portas --;
-    gpio_set_level(RD, 0);              // -- Zerando as portas --;
+    gpio_set_level(SH, 0);                  // -- Zerando as portas --;
+    gpio_set_level(CLK, 0);                 // -- Zerando as portas --;
+    gpio_set_level(WR, 0);                  // -- Zerando as portas --;
+    gpio_set_level(RD, 0);                  // -- Zerando as portas --;
 }
 
 // -- Criando a função de escrita e leitura do teclado --;
@@ -72,6 +72,6 @@ void app_main(void)
     {
         keyboardInit();
         varredura();
-        vTaskDelay(10); // -- Espera 10 milisegundos para proxima varredura --;
+        vTaskDelay(10);                         // -- Espera 10 milisegundos para proxima varredura --;
     }
 }
